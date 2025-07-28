25f2ad0fd0c2adadc6d7198e43efef08391e1c78
leewonjun67 <cm2003112423@gmail.com>
2025-07-28 21:46:20 +0900

    모르겠네요

---
+++ 알고리즘 스터디/6주차 문제풀이/실습/문제풀이5.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/70129
+
+#def solution(s):
+#    convert = 0
+#    zero = 0
+
+#    while True:
+#        if s == "1":
+#            break
+
+#        convert += 1
+
+#       zero_num = 0
+#        for n in s:
+#            if n == "0":
+#                zero_num += 1
+#        zero += zero_num

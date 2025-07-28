89193c8d8416b4480a01b4d5cc2c9437d1c9f4a9
leewonjun67 <cm2003112423@gmail.com>
2025-07-28 22:14:26 +0900

    dh

--- 알고리즘 스터디/6주차 문제풀이/실습/문제풀이3.py
+++ 알고리즘 스터디/6주차 문제풀이/실습/문제풀이3.py
@@ -1,11 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12906
 
-def solution(arr):
-    arr = arr[:]  
+def solution(arr): 
     i = 0
     while i < len(arr) - 1:
         if arr[i] == arr[i + 1]:
-            arr.pop(i + 1)
+            arr.pop(i + 1)                   # pop 함수는 리스트에서 마지막 요소 또는 특정 위치의 요소를 꺼내면서 제거하는 함수입니다.
             
         else:
             i += 1

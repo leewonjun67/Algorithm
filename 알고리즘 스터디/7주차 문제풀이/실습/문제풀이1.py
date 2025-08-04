a9032e838b74e7c74398e6ee0b90e9449a1ea355
leewonjun67 <cm2003112423@gmail.com>
2025-08-04 19:14:39 +0900

    8주차

--- 알고리즘 스터디/7주차 문제풀이/실습/문제풀이1.py
+++ 알고리즘 스터디/7주차 문제풀이/실습/문제풀이1.py
@@ -1,5 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12932
 
+# return list(int(str(n)[::-1]))
+
 def solution(n):
     nx = list(map(int, str(n)))
     a = len(nx)
@@ -10,3 +12,5 @@ def solution(n):
 
 n=12345
 print(solution(n))
+
+nx =[1,2,3,4,5]
